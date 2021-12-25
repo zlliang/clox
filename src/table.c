1deb0480d12953d57f780f85bd157136d45225f2
Zilong Liang <zlliang96@outlook.com>
2021-12-25 09:43:10 +0800

    Finish Chapter 30, and the total course!

--- src/table.c
+++ src/table.c
@@ -21,7 +21,7 @@ void freeTable(Table* table) {
 }
 
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
-  uint32_t index = key->hash % capacity;
+  uint32_t index = key->hash & (capacity - 1);
   Entry* tombstone = NULL;
 
   for (;;) {
@@ -37,7 +37,7 @@ static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
       return entry;
     }
 
-    index = (index + 1) % capacity;
+    index = (index + 1) & (capacity - 1);
   }
 }
 
@@ -114,7 +114,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length,
                            uint32_t hash) {
   if (table->count == 0) return NULL;
 
-  uint32_t index = hash % table->capacity;
+  uint32_t index = hash & (table->capacity - 1);
   for (;;) {
     Entry* entry = &table->entries[index];
     if (entry->key == NULL) {  // Stop if we find an empty non-tombstone entry.
@@ -124,7 +124,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length,
       return entry->key;
     }
 
-    index = (index + 1) % table->capacity;
+    index = (index + 1) & (table->capacity - 1);
   }
 }
 
--- src/value.c
+++ src/value.c
@@ -30,6 +30,17 @@ void freeValueArray(ValueArray* array) {
 }
 
 void printValue(Value value) {
+#ifdef NAN_BOXING
+  if (IS_BOOL(value)) {
+    printf(AS_BOOL(value) ? "true" : "false");
+  } else if (IS_NIL(value)) {
+    printf("nil");
+  } else if (IS_NUMBER(value)) {
+    printf("%g", AS_NUMBER(value));
+  } else if (IS_OBJ(value)) {
+    printObject(value);
+  }
+#else
   switch (value.type) {
     case VAL_BOOL:
       printf(AS_BOOL(value) ? "true" : "false");
@@ -44,9 +55,16 @@ void printValue(Value value) {
       printObject(value);
       break;
   }
+#endif
 }
 
 bool valuesEqual(Value a, Value b) {
+#ifdef NAN_BOXING
+  if (IS_NUMBER(a) && IS_NUMBER(b)) {
+    return AS_NUMBER(a) == AS_NUMBER(b);
+  }
+  return a == b;
+#else
   if (a.type != b.type) return false;
   switch (a.type) {
     case VAL_BOOL:
@@ -60,4 +78,5 @@ bool valuesEqual(Value a, Value b) {
     default:
       return false;  // Unreachable.
   }
+#endif
 }
